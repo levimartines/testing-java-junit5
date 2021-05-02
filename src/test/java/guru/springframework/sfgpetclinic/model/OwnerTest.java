7d9983409d3b6a471797b31c7447b60f4f7634b9
Levi Martines <levi.mferreira@gmail.com>
2021-05-01 21:15:54 -0300

    JUnit Dependent Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1L, "Levi", "Ferreira");
+        owner.setCity("Sorocaba");
+        owner.setAddress("Test Address, 999");
+        owner.setTelephone("555-5555");
+
+        assertAll("Properties teste",
+            () -> assertAll("Person properties",
+                () -> assertEquals("Levi", owner.getFirstName(), "First name didn't match"),
+                () -> assertEquals("Ferreira", owner.getLastName(), "Last name didn't match")),
+            () -> assertAll("Owner properties",
+                () -> assertEquals("Test Address, 999", owner.getAddress(), "Address didn't match"),
+                () -> assertEquals("555-5555", owner.getTelephone(), "Telephone didn't match"))
+            );
+    }
+}
