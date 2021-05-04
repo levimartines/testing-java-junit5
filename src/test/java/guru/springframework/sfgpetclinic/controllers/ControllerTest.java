7e97d135ff914fb21379f9d652eb9fbe9b6db9df
levimartines <levi.mferreira@gmail.com>
2021-05-04 11:19:07 -0300

    Default test method using @TestInstance

--- src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTest.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTest {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("BeforeAll Controller Test");
+    }
 
 }
