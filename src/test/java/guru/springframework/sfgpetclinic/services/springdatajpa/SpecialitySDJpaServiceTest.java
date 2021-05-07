45d88bb10b4c16c2db28e14eedfea9a209caf3f0
levimartines <levi.mferreira@gmail.com>
2021-05-07 14:27:30 -0300

    Returning values from mocks

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,13 +1,18 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,10 +25,19 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
+    void findById() {
+        Speciality speciality = new Speciality("dogs");
+        when(repository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+        Assertions.assertEquals("dogs", foundSpeciality.getDescription());
+    }
+
+    @Test
     void deleteById() {
         service.deleteById(1L);
         service.deleteById(1L);
-        verify(repository, Mockito.times(2)).deleteById(1L);
-        verify(repository, Mockito.never()).deleteById(2L);
+        verify(repository, times(2)).deleteById(1L);
+        verify(repository, never()).deleteById(2L);
     }
 }
